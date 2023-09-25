d0eb6b4fb4510592b7eae8f160f45844e133a63e
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-09-25 18:43:40 +0200

    Added routing for dozes

--- diabetApi/insulineDozes/views.py
+++ diabetApi/insulineDozes/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+
 
 # Create your views here.
+@csrf_exempt
+def dozesApi(request, id: int = 0):
+    if request.method == 'GET':
+        return JsonResponse("test" + str(id), safe=False)
