78b2860af3554279bc4c3062e2025b3cc71ee5e4
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-09-25 20:20:48 +0200

    Updated doze model

--- diabetApi/insulineDozes/models.py
+++ diabetApi/insulineDozes/models.py
@@ -7,6 +7,15 @@ from django.db import models
     
 class Doze(models.Model):
     Id = models.AutoField(primary_key=True)
-    units = models.IntegerField()
-    timestamp = models.DateTimeField()
-    description = models.CharField(max_length=300)
+    units = models.IntegerField(null=False, blank=False)
+    timestamp = models.DateTimeField(null=False, blank=False, unique=True)
+    description = models.CharField(max_length=300, blank=True, default='')
+    class InsulineType(models.TextChoices):
+        RAPID = "1", "RAPID-ACTING"
+        LONG = "2", "LONG-RUNNING"
+
+    type = models.CharField(
+        max_length=2,
+        choices=InsulineType.choices,
+        default=InsulineType.RAPID
+    )
--- diabetApi/insulineDozes/serializers.py
+++ diabetApi/insulineDozes/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Doze
 
+
 class DozeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doze
-        fields = ('Id', 'units', 'timestamp', 'description')
+        fields = ('Id', 'units', 'timestamp', 'description', 'type')
