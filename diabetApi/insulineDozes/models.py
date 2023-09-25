271749e61f883a277dac3bd12adf56c92bf19c0d
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-09-25 18:59:59 +0200

    Added model for doze.

--- diabetApi/insulineDozes/models.py
+++ diabetApi/insulineDozes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Create your models here.
+
+    
+class Doze(models.Model):
+    Id = models.AutoField(primary_key=True)
+    units = models.IntegerField()
+    timestamp = models.DateTimeField()
+    description = models.CharField(max_length=300)
