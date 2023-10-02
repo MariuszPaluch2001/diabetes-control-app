68db6491cd653d18ea682ce93e2579cfc5802788
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2023-10-02 23:45:54 +0200

    Added routing for meals and dishes

--- diabetFrontend/src/app/app-routing.module.ts
+++ diabetFrontend/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { SaveGlucoseFormComponent } from './glucose-level/save-form/save-form.co
 import { InsulineDozesComponent } from './insuline-dozes/insuline-dozes.component';
 import { InsulineSampleComponent } from './insuline-dozes/insuline-sample/insuline-sample.component';
 import { SaveInsulineFormComponent } from './insuline-dozes/save-form/save-form.component';
+import { DishesComponent } from './dishes/dishes.component';
+import { DishSampleComponent } from './dishes/dish-sample/dish-sample.component';
+import { MealsComponent } from './meals/meals.component';
+import { MealSampleComponent } from './meals/meal-sample/meal-sample.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
@@ -16,6 +20,10 @@ const routes: Routes = [
   { path: 'insuline-doze/list', component: InsulineDozesComponent },
   { path: 'insuline-doze/list/:id', component: InsulineSampleComponent },
   { path: 'insuline-doze/save-form', component: SaveInsulineFormComponent },
+  { path: 'dish/list', component: DishesComponent },
+  { path: 'dish/list/:id', component: DishSampleComponent },
+  { path: 'meal/list', component: MealsComponent },
+  { path: 'meal/list/:id', component: MealSampleComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
