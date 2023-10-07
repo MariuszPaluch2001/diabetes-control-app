af741fcabfc0ace66f15986c6bdf123f2371c8f1
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-10-07 17:22:22 +0200

    Added logging to django

--- diabetApi/core/diabetApi/urls.py
+++ diabetApi/core/diabetApi/urls.py
@@ -1,5 +1,11 @@
+import logging
+
 from django.contrib import admin
 from django.urls import include, path
+
+logger = logging.getLogger(__name__)
+logger.debug('This is a debug message')
+logger.info('This is an info message')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
