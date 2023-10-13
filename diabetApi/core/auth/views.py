5f8dcb7be2a24df50348ab92b264f6441f5dfdad
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-10-13 17:22:04 +0200

    Added authentication on backend

--- diabetApi/core/auth/views.py
+++ diabetApi/core/auth/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
-from django.http import Http404, HttpRequest
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.authentication import (SessionAuthentication,
+                                           TokenAuthentication)
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -17,17 +21,31 @@ class Register(APIView):
             user.set_password(request.data['password'])
             user.save()
             token = Token.objects.create(user=user)
-            return Response({"token": token.key, "user": serializer.data})
+            return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Login(APIView):
     def post(self, request: HttpRequest, format=None) -> Response:
-        return Response({})
+        user = get_object_or_404(User, username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
     
 class Logout(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request: HttpRequest, format=None) -> Response:
-        return Response({})
-    
+        request.user.auth_token.delete()
+        return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+
 class Test_Token(APIView):
-    def get(self, request: HttpRequest, id: int, format=None) -> Response:
-        return Response({})
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request: HttpRequest, format=None) -> Response:
+        content = {
+            'user': str(request.user),
+            'auth': str(request.auth),
+        }
+        return Response(content)
+    
+
--- diabetApi/core/diabetApi/settings/base.py
+++ diabetApi/core/diabetApi/settings/base.py
@@ -31,6 +31,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'core.diabetApi.urls'
 
 TEMPLATES = [
